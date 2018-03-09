9db3021a282abafca6512156399ae3068c2670d7
Carolina RC <caro.rmz315@gmail.com>
2018-03-08 23:21:19 -0600

    added buttons

---
+++ apps.js
@@ -0,0 +1,11 @@
+// Obtain a giphy API
+
+Semo8rtzISD2onGdZY0FO4MlSkdvTGz7
+
+// create and array of strings 
+var topics = [salsa, cumbia, merengue, bachata, kizomba];
+
+// when a user clicks on image it moves, then it stops if clicked on again
+// then it allows for user to create a button 
+// when the button is clicked it populates images 
+// once images are populated, the images will move upon first click and be still upon second click 
