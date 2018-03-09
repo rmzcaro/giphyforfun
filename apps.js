4431c2d5368f660bd069fead07f0c42b18c1c2b6
Carolina RC <caro.rmz315@gmail.com>
2018-03-08 23:49:37 -0600

    added API

--- apps.js
+++ apps.js
@@ -1,10 +1,24 @@
+$(document).ready(function() {
+    // alert("Hi!");
+});
+
 // Obtain a giphy API
 
-Semo8rtzISD2onGdZY0FO4MlSkdvTGz7
+    // Semo8rtzISD2onGdZY0FO4MlSkdvTGz7
 
-// create and array of strings 
-var topics = [salsa, cumbia, merengue, bachata, kizomba];
+// create and array of strings, * Try using a loop that appends a button for each string in the array.
 
+$("submit").click(function(){
+    $("#button-holder").append("<button>");
+});
+
+// get images 
+
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=salsa&api_key=Semo8rtzISD2onGdZY0FO4MlSkdvTGz7";
+
+var api = "https://api.giphy.com/v1/gifs/search";
+var apiKey = "Semo8rtzISD2onGdZY0FO4MlSkdvTGz7";
+var qty = "KEY&limit=5";
 // when a user clicks on image it moves, then it stops if clicked on again
 // then it allows for user to create a button 
 // when the button is clicked it populates images 
